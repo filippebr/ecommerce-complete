c6db4ee34aad53a6727b32ddac06171774b32abf
Filippe Braga <filippeffx@hotmail.com>
2023-06-30 23:44:25 -0300

    get users test server

--- server/src/__tests__/index.test.ts
+++ server/src/__tests__/index.test.ts
@@ -2,14 +2,15 @@ import supertest from 'supertest'
 import { describe, expect, test } from 'vitest'
 import app from '../server'
 
-describe('Testing server', () => {
+describe('GET /users', () => {
   // afterAll(() => app.close());
 
-  test('GET `/` route', async () => {
+  test('should return all users', async () => {
     await app.ready()
 
     const response = await supertest(app.server).get('/users')
 
     expect(response.statusCode).toEqual(200)
+    expect(response.text.length).toBeGreaterThan(0)
   })
 })
