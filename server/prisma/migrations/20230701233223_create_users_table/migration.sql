a906339d8e8a9110ac2a424f039d0c8a973a47e3
Filippe Braga <filippeffx@hotmail.com>
2023-07-01 20:36:17 -0300

    user model server

---
+++ server/prisma/migrations/20230701233223_create_users_table/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "mobile" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "isBlocked" BOOLEAN NOT NULL DEFAULT false,
+    "cart" TEXT,
+    "address" TEXT,
+    "refreshToken" TEXT NOT NULL,
+    "passwordChangeAt" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "passwordResetToken" TEXT,
+    "passwordResetExpires" DATETIME,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_mobile_key" ON "User"("mobile");
