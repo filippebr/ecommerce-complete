5aed40862f77f960e788fba86d595022fb05d1b1
Filippe Braga <filippeffx@hotmail.com>
2023-06-30 22:09:44 -0300

    first commit

---
+++ server/src/__tests__/index.test.ts
@@ -0,0 +1,18 @@
+import supertest from 'supertest'
+import { describe, expect, test } from 'vitest'
+import app from '../server'
+
+describe('Testing server', () => {
+  // afterAll(() => app.close());
+
+  test('GET `/` route', async () => {
+    await app.ready();
+  
+    const response = await supertest(app.server)
+      .post('/hello')
+      .expect(200)
+      
+    expect(response.body).toEqual({ hello: 'World'});
+  });
+})
+
---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+
+const server = fastify()
+
+// HTTP Method: GET, POST, PUT, PATCH, DELETE
+
+server.post('/hello', (request, reply) => {
+  reply.send({ hello: 'World'})
+})
+
+// API RESTful
+
+server.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP server running on http://localhost:3333')
+})
+
+export default server
