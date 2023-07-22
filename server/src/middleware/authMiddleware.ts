5396cbe5bdf6ddbb027ccccd113432ae11060be3
Filippe Braga <filippeffx@hotmail.com>
2023-07-22 19:19:23 -0300

    initial authMiddleware server

---
+++ server/src/middleware/authMiddleware.ts
@@ -0,0 +1,34 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import jwt, { Secret } from 'jsonwebtoken'
+import { prisma } from '../lib/prisma'
+
+export default async function authMiddleware(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  let token
+
+  if (request?.headers?.authorization?.startsWith('Bearer')) {
+    token = request.headers.authorization.split(' ')[1]
+    try {
+      if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as Secret)
+        console.log('decoded: ', decoded)
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id,
+          },
+        })
+
+        console.log('user: ', user)
+      }
+    } catch (error) {
+      reply.send({
+        message: 'Not authorized token expired. Please login again',
+      })
+    }
+  } else {
+    reply.send({ message: 'There is no token attached to header' })
+  }
+}
