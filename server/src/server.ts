34b502593666f5871833ac3c7d6b511c60fc389e
Filippe Braga <filippeffx@hotmail.com>
2023-07-22 23:10:06 -0300

    installed fastify-jwt

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,26 @@
-import fastify from 'fastify'
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import authMiddleware from './middleware/authMiddleware'
 import { errorHandler } from './middleware/errorHandler'
 import { authRoutes } from './routes/auth'
 
 const server = fastify()
+
+server.addHook('preHandler', async (request, reply, done) => {
+  // Your authMiddleware logic goes here
+  await authMiddleware(request, reply)
+  done()
+})
+
+server.decorate(
+  'authenticate',
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify()
+    } catch (err) {
+      reply.send(err)
+    }
+  },
+)
 
 server.setErrorHandler(errorHandler)
 
