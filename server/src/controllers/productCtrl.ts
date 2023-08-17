dccd23427cea55d31eba8fe3951b4747b0462149
Filippe Braga <filippeffx@hotmail.com>
2023-08-16 23:50:08 -0300

    create product route server

--- server/src/controllers/productCtrl.ts
+++ server/src/controllers/productCtrl.ts
@@ -1,6 +1,14 @@
 import { FastifyReply, FastifyRequest, RouteHandlerMethod } from 'fastify'
+import { prisma } from '../lib/prisma'
 
 export const createProduct: RouteHandlerMethod = async (
   request: FastifyRequest,
   reply: FastifyReply,
-) => {}
+) => {
+  try {
+    const newProduct = await prisma.product.create({})
+    return reply.send({ message: `Hey it's a product post route` })
+  } catch (error) {
+    return reply.send({ message: error })
+  }
+}
