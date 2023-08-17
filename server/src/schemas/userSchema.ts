cd7fb85143c22d5fc747f06c739922aba7417ca6
Filippe Braga <filippeffx@hotmail.com>
2023-08-17 19:00:14 -0300

    fix user schema server

--- server/src/schemas/userSchema.ts
+++ server/src/schemas/userSchema.ts
@@ -10,13 +10,13 @@ const userSchema = z.object({
       required_error: 'Firstname is required',
       invalid_type_error: 'Firstname must be a string',
     })
-    .nonempty(),
+    .min(1),
   lastname: z
     .string({
       required_error: 'Lastname is require',
       invalid_type_error: 'Lastname must be a string',
     })
-    .nonempty(),
+    .min(1),
   mobile: z.string().min(6).max(14),
   password: z.string().min(6),
   email: z
@@ -24,12 +24,9 @@ const userSchema = z.object({
       required_error: 'Email is required',
     })
     .email({ message: 'Invalid email address' }),
-  role: z
-    .string()
-    .nonempty()
-    .refine((value) => validRoles.includes(value), {
-      message: 'Invalid role value',
-    }),
+  role: z.string().refine((value) => validRoles.includes(value), {
+    message: 'Invalid role value',
+  }),
   isBlocked: z.boolean().optional(),
   address: z.string().optional(),
   cart: z.array(cartSchema).optional(),
